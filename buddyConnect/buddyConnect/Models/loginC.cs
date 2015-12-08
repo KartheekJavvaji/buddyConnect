5e0889c98d5e63bc1dd92f67a42967f3c192ad61
Kartheek J <kartheekjavvaji@live.com>
2015-12-08 15:19:14 +0530

    added login model

---
+++ buddyConnect/buddyConnect/Models/loginC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buddyConnect.Models
+{
+    public class loginC
+    {
+        public string result { get; set; }
+        public string output { get; set; }
+        public string sno { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string age { get; set; }
+        public string createddate { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string status { get; set; }
+        public string email { get; set; }
+        public string friends_list { get; set; }
+        public string profile_imgurl { get; set; }
+        public string intrests { get; set; }
+        public string addictional_information { get; set; }
+        public string looking_for { get; set; }
+        public string body_type { get; set; }
+        public string drinks { get; set; }
+        public string height { get; set; }
+        public string gender { get; set; }
+        public string points { get; set; }
+    }
+}
--- buddyConnect/buddyConnect/login.xaml.cs
+++ buddyConnect/buddyConnect/login.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +24,18 @@ namespace buddyConnect
     /// </summary>
     public sealed partial class login : Page
     {
+        private HttpClient httpClient;
+        private HttpResponseMessage responseMes;
+
+        
         public login()
         {
             this.InitializeComponent();
+            httpClient = new HttpClient();
+            var headers = httpClient.DefaultRequestHeaders;
+            headers.UserAgent.ParseAdd("ie");
+            headers.UserAgent.ParseAdd("Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
+
         }
 
         private void username_TextChanged(object sender, TextChangedEventArgs e)
@@ -43,13 +54,42 @@ namespace buddyConnect
                 loginButton.IsEnabled = false;
         }
 
-        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        private void newUser_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void newUser_Click(object sender, RoutedEventArgs e)
+        private async void loginButton_Click(object sender, RoutedEventArgs e)
         {
+            responseMes = new HttpResponseMessage();
+
+            // The value of 'InputAddress' is set by the user and is therefore untrusted input. 
+            // If we can't create a valid URI, 
+            // We notify the user about the incorrect input.
+
+            string responseBodyAsText;
+
+
+            try
+            {
+                responseMes = await httpClient.GetAsync("https://api.nasa.gov/planetary/apod?api_key=RioAxhYhTzbsQYi7ufl2JW69FfowN4GyRHLeOkM4&format=JSON");
+
+                responseMes.EnsureSuccessStatusCode();
+
+                responseBodyAsText = await responseMes.Content.ReadAsStringAsync();
+
+            }
+            catch (Exception ex)
+            {
+                // Need to convert int HResult to hex string
+                res.Text = "Error = " + ex.HResult.ToString("X") +
+                    "  Message: " + ex.Message;
+                responseBodyAsText = "";
+            }
+
+            apodObj = JsonConvert.DeserializeObject<apod>(responseBodyAsText);
+            stats.Text = responseMes.StatusCode + " " + responseMes.ReasonPhrase;
+            DataContext = apodObj;
 
         }
     }
