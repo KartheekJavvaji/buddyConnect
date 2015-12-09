26646c8477a23d39c82eba962a203c6623b8db91
Kartheek J <kartheekjavvaji@live.com>
2015-12-09 15:45:34 +0530

    added VMs

---
+++ buddyConnect/buddyConnect/View Models/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buddyConnect.View_Models
+{
+    public class BaseViewModel : ViewModelBase
+    {
+
+        private bool isLoading;
+        public virtual bool IsLoading
+        {
+            get { return isLoading; }
+            set
+            {
+                isLoading = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
--- buddyConnect/buddyConnect/homeviews/buddies.xaml.cs
+++ buddyConnect/buddyConnect/homeviews/buddies.xaml.cs
@@ -11,31 +11,20 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using buddyConnect.homeviews;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace buddyConnect
+namespace buddyConnect.homeviews
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class home : Page
+    public sealed partial class buddies : Page
     {
-        public home()
+        public buddies()
         {
             this.InitializeComponent();
-        }
-
-        private void chatsClick(object sender, RoutedEventArgs e)
-        {
-            homeFrame.Navigate(typeof(chats));
-        }
-
-        private void friendListClick(object sender, RoutedEventArgs e)
-        {
-            homeFrame.Navigate(typeof(buddies));
         }
     }
 }
